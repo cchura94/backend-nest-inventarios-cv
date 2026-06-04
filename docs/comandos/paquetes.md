3c9427be1c218457723cb3c237348987de243d7a
Cristian <cchura.cpc@gmail.com>
2026-06-03 22:55:17 -0400

    add swagger

---
+++ docs/comandos/paquetes.md
@@ -0,0 +1,16 @@
+- Para instalar nuevos paquetes con (pnpm)
+```
+pnpm add nombre-paquete
+```
+- Si queremos instalar swagger
+-  npm install --save @nestjs/swagger
+con pnpm:
+```
+pnpm add @nestjs/swagger
+pnpm approve-builds
+```
+
+Para configurar variables de entorno:
+```
+pnpm add @nestjs/config
+```
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend Api')
+    .setDescription('Api rest')
+    .setVersion('1.0')
+    .addTag('backend')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+  // endswagger
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
